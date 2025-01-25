4d95191a4408c16253e626e06cbc9a03a145e861
Abraham Adeniyi <adeniyiabraham74@gmail.com>
2025-01-25 17:47:17 -0500

    Added a time machine controller

--- 5125Assignment1/Controllers/TimeMachineController.cs
+++ 5125Assignment1/Controllers/TimeMachineController.cs
@@ -6,13 +6,39 @@ namespace _5125Assignment1.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class TimeMachineController : ControllerBase
-    {
-        [HttpGet(template: "/api/q7/timemachine")]
-        [Consumes("application/x-www-form-urlencoded")]
-
-        public string TimeMachine([FromForm]int days)
+    {/// <summary>
+     /// Returns a string representation of the current date (formatted yyyy-MM-dd), adjusted by {days}
+     /// </summary>
+     /// 
+     /// <param name="days">
+     /// Number of days to convert to date
+     /// </param>
+     /// 
+     /// <example>
+     /// GET https://localhost:xx/api/q7/timemachine/1
+     /// -> 2000-01-02
+     /// </example>
+     /// 
+     /// <example>
+     /// GET https://localhost:xx/api/q7/timemachine/-1
+     /// -> 1999-12-31
+     /// </example>
+        [HttpGet(template: "/api/q7/timemachine/{days}")]
+        public string TimeMachine(int days)
         {
-            return "yel";
+            int numberOfDays = days;
+
+            var getDate = new DateTime(2000, 1, 1);
+            var daysToDate = getDate.AddDays(numberOfDays);
+
+            return daysToDate.ToString("yyyy-MM-dd");
+
+            /// youtube video reference
+            /// Video title: 17: DateTime in C# | Creating Dates and Times in C# | C# Tutorial For Beginners | C Sharp Tutorial
+            /// url: https://www.youtube.com/watch?v=KKzSQ6r93dY
+            /// 
+
+
         }
     }
 }
